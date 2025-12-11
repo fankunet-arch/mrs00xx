22449577355580969927a90685ba3adf73ad31d3
fankunet-arch <fankunet@gmail.com>
2025-12-11 23:15:49 +0100

    Add files via upload

--- docs/mrsexp_db_schema_structure_only.sql
+++ docs/mrsexp_db_schema_structure_only.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- 主机： mhdlmskp2kpxguj.mysql.db
--- 生成日期： 2025-12-11 22:21:38
+-- 生成日期： 2025-12-11 23:12:47
 -- 服务器版本： 8.4.6-6
 -- PHP 版本： 8.1.33
 
@@ -75,6 +75,7 @@ CREATE TABLE `express_package` (
   `package_status` enum('pending','verified','counted','adjusted') COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'pending' COMMENT '包裹状态',
   `content_note` text COLLATE utf8mb4_unicode_ci COMMENT '内容备注（清点时填写）',
   `expiry_date` date DEFAULT NULL COMMENT '保质期（非生产日期，选填）',
+  `quantity` int UNSIGNED DEFAULT NULL COMMENT '数量（选填）',
   `adjustment_note` text COLLATE utf8mb4_unicode_ci COMMENT '调整备注',
   `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
   `verified_at` datetime DEFAULT NULL COMMENT '核实时间',
